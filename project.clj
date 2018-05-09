65d374d60dcf4cef10676646f1c6400cd508fd0f
CharlesHD <charleshdespointes@gmail.com>
2018-05-09 10:43:52 +0200

    Project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject re-cytoscape "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Working with cytoscape.js via re-frame"
+  :url "http://github.com/CharlesHD/re-cytoscape"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
