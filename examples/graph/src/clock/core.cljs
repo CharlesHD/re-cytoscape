7fa103a957d6216d203054ac43e745b6d72ada7f
Charles Huyghues-Despointes <charleshdespointes@gmail.com>
2018-05-17 00:19:01 +0200

    progress

--- examples/graph/src/clock/core.cljs
+++ examples/graph/src/clock/core.cljs
@@ -12,12 +12,29 @@
    (assoc db :text txt)))
 
 ;; graph helper
+(defn parse-line
+  [line]
+  (let [splt (map str/trim (str/split line #"--"))]
+    (if (= (count splt) 2)
+      {:data {:id line :source (first splt) :target (last splt)}}
+      {:data {:id (first splt)}})))
+
+(defn text->graph
+  [txt]
+  (for [x (str/split txt #"\n")]
+    (parse-line x)))
 
 ;; subscription
 (rf/reg-sub
  :graph-text
  (fn [db _]
    (:text db)))
+
+(rf/reg-sub
+ :graph-elements
+ :<- [:graph-text]
+ (fn [txt _]
+   (text->graph txt)))
 
 ;; Visualisation
 (defn graph-text []
